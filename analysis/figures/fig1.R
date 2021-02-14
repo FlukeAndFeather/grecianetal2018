0300523993d4cfd50c800aaef4cd68b7272ce097
Max Czapanskiy <maxczap@stanford.edu>
2021-02-14 00:04:38 -0800

    Calculating UDs and mapping them. Next up: bathymetry and scalebar.

--- analysis/figures/fig1.R
+++ analysis/figures/fig1.R
@@ -1,7 +1,10 @@
+library(adehabitatHR)
+library(raster)
 library(rnaturalearth)
 library(sf)
 library(tidyverse)
 
+# Load track data
 adu_tracks_df <- "analysis/data/raw_data/gannet_gps_locs_2015_adults.csv" %>%
   read_csv() %>%
   mutate(age = "adult")
@@ -10,36 +13,63 @@ imm_tracks_df <- "analysis/data/raw_data/gannet_gps_locs_2015_immatures.csv" %>%
   mutate(age = "immature")
 all_tracks_df <- rbind(adu_tracks_df, imm_tracks_df)
 
-coords_to_lines <- function(x, y) {
-  stopifnot(
-    is.numeric(x),
-    is.numeric(y),
-    length(x) == length(y)
-  )
+# Have points already been regularized?
+all_tracks_df %>%
+  group_by(trip) %>%
+  mutate(dt = as.numeric(lead(datetime_utc) - datetime_utc, unit = "mins")) %>%
+  ungroup() %>%
+  summarize_at(vars(dt), list(min, max, mean), na.rm = TRUE)
+# Looks good!
+# # A tibble: 1 x 3
+#     fn1   fn2   fn3
+#   <dbl> <dbl> <dbl>
+# 1     2     2     2
 
-  list(st_linestring(cbind(x, y)))
+# adehabitatHR::kernelUD() takes SpatialPoints objects
+# run kernelUD() once (instead of per-age class) so results end up on same grid
+
+# UTM 30N crs
+utm_30n <- CRS("+init=EPSG:32630")
+
+uds <- all_tracks_df %>%
+  {SpatialPointsDataFrame(coords = cbind(.$UTM_x, .$UTM_y),
+                          data = data.frame(id = .$age),
+                          proj4string = utm_30n)} %>%
+  # Methods specify 1 km grid, 10 km smoothing parameter
+  kernelUD(grid = 1e3, h = 10e3)
+
+ud_levels <- c(25, 50, 75, 95)
+ud_to_sf <- function(percent) {
+  getverticeshr(uds, percent) %>%
+    st_as_sf() %>%
+    mutate(ud_level = paste0(percent, "%"))
 }
+hrs <- map_dfr(ud_levels, ud_to_sf) %>%
+  rename(age = id) %>%
+  mutate(ud_level = factor(ud_level, levels = c("95%", "75%", "50%", "25%")))
 
-all_tracks_sf <- all_tracks_df %>%
-  group_by(id, trip, age) %>%
-  summarize(geometry = coords_to_lines(lon, lat),
-            .groups = "drop") %>%
-  st_as_sf(crs = "EPSG:4326")
 
-land50 <- ne_download(scale = 50,
-                      type = "land",
-                      category = "physical",
-                      returnclass = "sf") %>%
-  st_crop(xmin = -5,
-          ymin = 50,
-          xmax = 10,
-          ymax = 65)
+# Land shapefiles
+fig1_extent <- extent(hrs) * 1.2
+land_sf <- ne_download(scale = "large",
+                       type = "land",
+                       category = "physical",
+                       returnclass = "sf") %>%
+  st_transform(utm_30n)
 
-ggplot(all_tracks_sf) +
-  geom_sf(data = land50,
-          fill = "grey60",
-          color = NA) +
-  geom_sf(aes(color = age)) +
-  coord_sf(xlim = st_bbox(all_tracks_sf)[c("xmin", "xmax")],
-           ylim = st_bbox(all_tracks_sf)[c("ymin", "ymax")]) +
-  facet_grid(cols = vars(age))
+# Fig 1a
+ggplot() +
+  geom_sf(data = land_sf, fill = "#989898", color = NA) +
+  geom_sf(aes(fill = ud_level), data = hrs, color = NA) +
+  scale_x_continuous(breaks = c(-5, 0, 5)) +
+  scale_y_continuous(breaks = seq(52, 60, by = 2)) +
+  scale_fill_manual(values = c(`25%` = "#DB602B",
+                               `50%` = "#E9C14C",
+                               `75%` = "#FFFF5A",
+                               `95%` = "#D5D5D5")) +
+  labs(fill = "UD") +
+  guides(fill = guide_legend(reverse = TRUE)) +
+  coord_sf(xlim = fig1_extent[1:2],
+           ylim = fig1_extent[3:4]) +
+  facet_grid(cols = vars(age)) +
+  theme_minimal()
